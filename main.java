9c69aa7bc388efb75a61e97a430fa27ad972f48f
GroovyChocobo <elizabeth8.taiwo@gmail.com>
2026-04-28 09:07:49 -0400

    used scanner imput to ask the user which city's forecast they want to view.

--- main.java
+++ main.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 import java.util.URL;
 
-public class WeatherApppublic class WeatherApp{
+public class WeatherApp{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a city name: ");
+        String city = input.nextLine();
+
+        String
     }
 }
